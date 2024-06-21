13a868da6b76018bdede9b1deb21a5ffc6bd79a3
Yizack Rangel <yizackr@gmail.com>
2024-06-21 01:48:05 -0500

    chore: add webhook validator for GitHub

---
+++ src/runtime/server/lib/validators/github.ts
@@ -0,0 +1,27 @@
+import { subtle } from 'node:crypto'
+import { Buffer } from 'node:buffer'
+import { type H3Event, getHeaders, readRawBody } from 'h3'
+import { useRuntimeConfig } from '#imports'
+
+export const isValidGithubWebhook = async (event: H3Event): Promise<boolean> => {
+  const headers = getHeaders(event)
+  const body = await readRawBody(event)
+  const { secretKey } = useRuntimeConfig(event).webhook.github
+
+  const GITHUB_SIGNATURE = 'X-Hub-Signature-256'.toLowerCase()
+  const header = headers[GITHUB_SIGNATURE]
+
+  if (!body || !header) return false
+
+  const parts = header.split('=')
+  const webhookSignature = parts[1]
+
+  const encoder = new TextEncoder()
+  const algorithm = { name: 'HMAC', hash: 'SHA-256' }
+
+  const extractable = false
+  const key = await subtle.importKey('raw', encoder.encode(secretKey), algorithm, extractable, ['verify'])
+  const isValid = await subtle.verify(algorithm.name, key, Buffer.from(webhookSignature, 'hex'), encoder.encode(body))
+
+  return isValid
+}
--- src/runtime/server/utils/webhooks.ts
+++ src/runtime/server/utils/webhooks.ts
@@ -1,3 +1,4 @@
+export { isValidGithubWebhook } from '../lib/validators/github'
 export { isValidPaddleWebhook } from '../lib/validators/paddle'
 export { isValidPaypalWebhook } from '../lib/validators/paypal'
 export { isValidTwitchWebhook } from '../lib/validators/twitch'
