6f8f84b28160daf55edd2bde360d6bf4b673852a
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-05 07:08:09 -0500

    chore: add isValidWebhook attr

--- playground/server/api/webhooks/discord.post.ts
+++ playground/server/api/webhooks/discord.post.ts
@@ -8,7 +8,7 @@ export default defineEventHandler(async (event) => {
  * The PING message is used during the initial webhook handshake, and is
    required to configure the webhook in the developer portal.
   */
-  if (body.type === 1) return { type: body.type } // PING
+  if (body.type === 1) return { type: body.type, isValidWebhook } // PING
 
   return { isValidWebhook }
 })
