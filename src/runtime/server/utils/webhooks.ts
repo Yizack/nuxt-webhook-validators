6407f9e3f502ee9686840d22202e6aefc3c25c2b
Yizack Rangel <yizackr@gmail.com>
2024-07-05 18:40:53 -0500

    chore: sort exports

--- src/runtime/server/utils/webhooks.ts
+++ src/runtime/server/utils/webhooks.ts
@@ -1,7 +1,7 @@
+export { isValidDiscordWebhook } from '../lib/validators/discord'
 export { isValidGithubWebhook } from '../lib/validators/github'
 export { isValidPaddleWebhook } from '../lib/validators/paddle'
 export { isValidPaypalWebhook } from '../lib/validators/paypal'
 export { isValidTwitchWebhook } from '../lib/validators/twitch'
 export { isValidStripeWebhook } from '../lib/validators/stripe'
 export { isValidNuxthubWebhook } from '../lib/validators/nuxthub'
-export { isValidDiscordWebhook } from '../lib/validators/discord'
