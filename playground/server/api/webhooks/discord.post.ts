be0a4c3e1eaae7911e2b0a0aa67783926b806706
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-05 06:58:35 -0500

    chore: interaction endpoint handle initial configuration

--- playground/server/api/webhooks/discord.post.ts
+++ playground/server/api/webhooks/discord.post.ts
@@ -1,9 +1,14 @@
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-  console.log(body)
-
   const isValidWebhook = await isValidDiscordWebhook(event)
   if (!isValidWebhook) throw createError({ statusCode: 401, message: 'Unauthorized: webhook is not valid' })
+  
+  const body = await readBody(event)
+  
+  /**
+ * The PING message is used during the initial webhook handshake, and is
+   required to configure the webhook in the developer portal.
+  */
+  if (body.type === 1) return body.type // PING
 
   return { isValidWebhook }
 })
