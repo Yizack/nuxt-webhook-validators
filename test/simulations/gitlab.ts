1c44bc085688edbc488763db49ecc002f1d0576e
Yizack Rangel <yizackr@gmail.com>
2025-02-02 19:43:43 -0500

    test(gitlab): no sign needed

--- test/simulations/gitlab.ts
+++ test/simulations/gitlab.ts
@@ -1,19 +1,11 @@
-import { subtle } from 'node:crypto'
-import { Buffer } from 'node:buffer'
 import { $fetch } from '@nuxt/test-utils/e2e'
-import { encoder, HMAC_SHA256 } from '../../src/runtime/server/lib/helpers'
 import nuxtConfig from '../fixtures/basic/nuxt.config'
 
 const body = 'testBody'
 const secretToken = nuxtConfig.runtimeConfig?.webhook?.gitlab?.secretToken
 
 export const simulateGitLabEvent = async () => {
-  const signature = await subtle.importKey('raw', encoder.encode(secretToken), HMAC_SHA256, false, ['sign'])
-  const hmac = await subtle.sign(HMAC_SHA256.name, signature, encoder.encode(body))
-  const computedHash = Buffer.from(hmac).toString('hex')
-  const validSignature = `sha256=${computedHash}`
-
-  const headers = { 'X-Gitlab-Token': validSignature }
+  const headers = { 'X-Gitlab-Token': secretToken! }
 
   return $fetch<{ isValidWebhook: boolean }>('/api/webhooks/gitlab', {
     method: 'POST',
