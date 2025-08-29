249540c6975a0a4f97982b34b8f4732c0462a9b3
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-29 01:25:51 -0500

    chore(playground): add twitch webhook callback verification (#13)
    * chore(playground): add twitch webhook callback verification
    
    * [autofix.ci] apply automated fixes
    
    * fix(test): twitch simulation

--- test/simulations/twitch.ts
+++ test/simulations/twitch.ts
@@ -4,13 +4,13 @@ import { $fetch } from '@nuxt/test-utils/e2e'
 import { encoder, HMAC_SHA256 } from '../../src/runtime/server/lib/helpers'
 import nuxtConfig from '../fixtures/basic/nuxt.config'
 
-const body = 'testBody'
+const body = { data: 'testBody' }
 const messageId = 'testMessageId'
 const secretKey = nuxtConfig.runtimeConfig?.webhook?.twitch?.secretKey
 
 export const simulateTwitchEvent = async () => {
   const timestamp = Math.floor(Date.now() / 1000)
-  const message = messageId + timestamp + body
+  const message = messageId + timestamp + JSON.stringify(body)
   const signature = await subtle.importKey('raw', encoder.encode(secretKey), HMAC_SHA256, false, ['sign'])
   const hmac = await subtle.sign(HMAC_SHA256.name, signature, encoder.encode(message))
   const computedHash = Buffer.from(hmac).toString('hex')
